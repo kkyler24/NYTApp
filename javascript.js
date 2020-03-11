d1e9ff50ab5570e972e24cedaeb14ce47daa058a
Ky Kyler <ky.kyler1@gmail.com>
2020-03-11 11:08:11 -0400

    slide up code

--- javascript.js
+++ javascript.js
@@ -1,11 +1,4 @@
-newFunction();
-
-function newFunction() {
-    $("button").on("click", function () {
-        console.log("we faded");
-        $('div').fadeOut(1000, function () {
-            $(this).remove();
-        });
-    });
-}
+$("button").on("click", function(){
+    $('div').slideUp();
+});
 
