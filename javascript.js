730019854a891011cd9072187c087e290ddadfe3
Ky Kyler <ky.kyler1@gmail.com>
2020-03-10 20:48:57 -0400

    updated and changed faded code

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
+newFunction();
+
+function newFunction() {
+    $("button").on("click", function () {
+        console.log("we faded");
+        $('div').fadeOut(1000, function () {
+            $(this).remove();
+        });
+    });
+}
 
